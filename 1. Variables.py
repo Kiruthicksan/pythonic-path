5c943e0752807343c5a7e7321069438e97237ffa
Kiruthicksan <kiruthicksanboopathi@gmail.com>
2025-08-31 17:35:53 +0530

    Variable and numbers

---
+++ 1. Variables.py
@@ -0,0 +1,14 @@
+age = 22
+
+
+
+print("My age is", age)
+
+
+a = 7
+b = 15
+c = a+b
+
+
+if(age == c):
+    print("Wow, What a coincidence")
---
+++ 2. Numbers.py
@@ -0,0 +1,20 @@
+# Addition
+
+sum = 5 + 3
+
+print(sum)
+
+# Subrating
+differnce =  5-1  # do you know the answer
+
+# Multiplication
+
+product  = 5 * 5 
+print(product)
+
+# Division
+
+division = 10 / 2   # 5.0
+print(division)
+division1 = 10 // 2    # 5
+print(division1)
