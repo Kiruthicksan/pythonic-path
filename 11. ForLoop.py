227eac4cf41b775405aee7624bfe13293de8bf6a
Kiruthicksan <kiruthicksanboopathi@gmail.com>
2025-09-01 09:03:17 +0530

    Loops

---
+++ 11. ForLoop.py
@@ -0,0 +1,14 @@
+for i in range(10):
+    print("I am a Calm Person")
+
+
+for i in range(2,11,2):
+    print(i)
+
+
+# sting
+
+word = "python"
+
+for char in word:
+    print(char)
---
+++ 12. Whileloop.py
@@ -0,0 +1,6 @@
+i = 1
+
+while i < 10:
+    print("It is less")
+    i += 1
+    
