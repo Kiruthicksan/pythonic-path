d1eba95237bee558fd5b497b9aa2e63ae5d8028a
Kiruthicksan <kiruthicksanboopathi@gmail.com>
2025-09-01 10:42:56 +0530

    lists

---
+++ 13. List.py
@@ -0,0 +1,17 @@
+my_list = ["kiruthick", 20, "calm", 3.14, "Emotionally Intelligence", "Fearless"]
+
+print(my_list[2])
+
+#accessing
+
+print(my_list[2::2])
+
+# updating
+my_list[1] = 22
+
+# deleting
+
+del my_list[3]
+
+print(my_list)
+
---
+++ 14. dimensions in lists.py
@@ -0,0 +1,15 @@
+# 2d list
+
+
+matrix = [[1, 2, 3], [4, 3, 1]]
+
+print(matrix)
+
+
+# 3d list
+
+
+cube = [1, 2, 3, [2, 45, [1, 2, 3]], 3, 3]  # this is not typically 3dlist
+
+og_cube = [[[2, 3, 4]],[[2, 3, 4]]]
+print(cube)
